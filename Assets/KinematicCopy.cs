9f67fc6fe6e7c8999dde7cd57c5221aa9f9a467b
s-ol <s-ol@users.noreply.github.com>
2016-12-11 00:50:27 +0100

    decouple movement

---
+++ Assets/KinematicCopy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KinematicCopy : MonoBehaviour {
+	public Rigidbody2D originalBody;
+	private Rigidbody2D ownBody;
+
+	void Start () {
+		ownBody = GetComponent<Rigidbody2D> ();
+	}
+	
+	void FixedUpdate () {
+		ownBody.MovePosition (originalBody.transform.position);
+		ownBody.MoveRotation (originalBody.transform.rotation.eulerAngles.z);
+	}
+}
