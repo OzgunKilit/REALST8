3e789a17d9d9777b6b76c808be4a47c919aabb46
s-ol <s-ol@users.noreply.github.com>
2016-12-11 00:08:41 +0100

    aCCeler8/in

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -3,43 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
-
 	public WheelJoint2D leftWheelJoint, rightWheelJoint;
 	private JointMotor2D jointMotor;
+	private JointMotor2D leftJointMotor, rightJointMotor;
 
 	public float motorSpeed = 300;
+	public float maxSpeed = 500;
 
-	// Use this for initialization
 	void Start () {
 		jointMotor = leftWheelJoint.motor;
+		leftJointMotor = leftWheelJoint.motor;
+		rightJointMotor = rightWheelJoint.motor;
 	}
 
-	// Update is called once per frame
 	void FixedUpdate () {
+		float rightCtrl = 0;
+		if (Input.GetKey (KeyCode.LeftArrow))
+			rightCtrl -= 1;
+		if (Input.GetKey (KeyCode.RightArrow))
+			rightCtrl += 1;
 
-		jointMotor.motorSpeed = 0;
-		leftWheelJoint.motor = jointMotor;
-		jointMotor.motorSpeed = 0;
-		rightWheelJoint.motor = jointMotor;
+		rightJointMotor.motorSpeed = Mathf.Clamp (rightJointMotor.motorSpeed + rightCtrl  * maxSpeed * Time.fixedDeltaTime, -maxSpeed, maxSpeed);
 
+		if (rightCtrl == 0)
+			rightJointMotor.motorSpeed *= 0.9f;
+		if (Mathf.Abs(rightJointMotor.motorSpeed) < 10)
+			rightJointMotor.motorSpeed = 0;
+		rightWheelJoint.motor = rightJointMotor;
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			jointMotor.motorSpeed = motorSpeed;
-			rightWheelJoint.motor = jointMotor;
-		}
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			jointMotor.motorSpeed = -motorSpeed;
-			rightWheelJoint.motor = jointMotor;
-		}
-		if (Input.GetKey (KeyCode.D)) {
-			jointMotor.motorSpeed = motorSpeed;
-			leftWheelJoint.motor = jointMotor;
-		}
-		if (Input.GetKey (KeyCode.A)) {
-			jointMotor.motorSpeed = -motorSpeed;
-			leftWheelJoint.motor = jointMotor;
-		}
+		float leftCtrl = 0;
+		if (Input.GetKey (KeyCode.A))
+			leftCtrl -= 1;
+		if (Input.GetKey (KeyCode.D))
+			leftCtrl += 1;
+		leftJointMotor.motorSpeed = Mathf.Clamp (leftJointMotor.motorSpeed + leftCtrl * maxSpeed * Time.fixedDeltaTime, -maxSpeed, maxSpeed);
 
-
+		if (leftCtrl == 0)
+			leftJointMotor.motorSpeed *= 0.9f;
+		if (Mathf.Abs(leftJointMotor.motorSpeed) < 10)
+			leftJointMotor.motorSpeed = 0;
+		leftWheelJoint.motor = leftJointMotor;
 	}
 }
