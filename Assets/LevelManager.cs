8dc6254c5b7edfa762578e9915347788468a6fd4
Ozgun Kilit <ozgunkilit@gmail.com>
2016-12-11 12:29:13 -0500

    healthz

--- Assets/LevelManager.cs
+++ Assets/LevelManager.cs
@@ -1,17 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LevelManager : MonoBehaviour {
 
 	public float health = 100;
+
+	private Text currentHealthText;
 	// Use this for initialization
 	void Start () {
-		
+		currentHealthText = GameObject.Find ("CurrentHealth").GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		int currentHealth = int.Parse (currentHealthText.text);
+		currentHealth = (int)health;
+		currentHealthText.text = currentHealth.ToString ();
+
 		
 	}
 }
