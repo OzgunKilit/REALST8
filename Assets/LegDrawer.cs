76679abd740ca03524258bcdcd72545504820a42
s-ol <s-ol@users.noreply.github.com>
2016-12-12 19:45:06 +0100

    fix legDrawer to work via hierarchy

--- Assets/LegDrawer.cs
+++ Assets/LegDrawer.cs
@@ -3,23 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LegDrawer : MonoBehaviour {
-	public Rigidbody2D house;
-	public Vector2 hip, foot;
+	public Transform hip, foot;
 
 	private Vector2 knee, velocity;
 	private LineRenderer lines;
 
 	void Start () {
 		lines = GetComponent<LineRenderer> ();
-
-		hip = house.transform.InverseTransformPoint (hip);
-		foot = transform.InverseTransformPoint (foot);
-
 		knee = getOptimalPosition ();
 	}
 
 	Vector2 getOptimalPosition() {
-		return (house.transform.TransformPoint(hip) + transform.TransformPoint(foot))/2;
+		return (hip.position + foot.position)/2;
 	}
 
 	void Update () {
@@ -27,9 +22,9 @@ public class LegDrawer : MonoBehaviour {
 		knee += velocity * Time.deltaTime;
 		velocity *= 0.9f;
 
-		Vector3 a = house.transform.TransformPoint (hip);
-		Vector3 b = knee; //getOptimalPosition ();
-		Vector3 c = transform.TransformPoint (foot);
+		Vector3 a = hip.position;
+		Vector3 b = knee;
+		Vector3 c = foot.position;
 
 		for (int i = 0; i < lines.numPositions; i++)
 			lines.SetPosition (i, Bezier.GetPoint(a, b, b, c, i / (float)(lines.numPositions-1)));
@@ -39,8 +34,8 @@ public class LegDrawer : MonoBehaviour {
 		Gizmos.color = Color.yellow;
 		Gizmos.DrawWireSphere (knee, 0.05f);
 		Gizmos.color = Color.green;
-		Gizmos.DrawWireSphere (house.transform.TransformPoint(hip), 0.05f);
+		Gizmos.DrawWireSphere (hip.position, 0.05f);
 		Gizmos.DrawWireSphere (getOptimalPosition(), 0.05f);
-		Gizmos.DrawWireSphere (transform.TransformPoint(foot), 0.05f);
+		Gizmos.DrawWireSphere (foot.position, 0.05f);
 	}
 }
